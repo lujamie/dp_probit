88fd9e83872d8fd256984ca82d4272a711150fa0
Jamie Lu <jamielu@college.harvard.edu>
2023-10-24 14:15:14 -0400

    rr and shuffling

--- experiments.py
+++ experiments.py
@@ -4,111 +4,226 @@ error metric is the log likelihood of estimators
 tries different values of the privacy parameter
 '''
 
+from unittest import skip
 from generate_data import generate, torch_data
-from mechanisms import local_budget, predict, probit_err, privatize, run_probit
+from mechanisms import local_budget_renyi, noise_params, predict, privatize, run_probit, sampling_params, sampling_privatize, sample_sz
+from priv_opt import optimize
 from sgd import dp_sgd
+import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 # initialize parameters
-K = 10   # dimensions
-N = 400  # sample size
-num_trials = 1
-error = "PRED"  # error metric: LOG-LIKE, BETA-NORM, PRED
+d = 5   # dimensions
+N = 1000  # sample size
+num_trials = 5
+# error = "PRED"  # error metric: LOG-LIKE, BETA-NORM, PRED
 
 # privacy parameters
-eps = np.arange(start=0.1, stop=6, step=0.2)    # overall epsilon values after shuffling
+eps = np.arange(start=1, stop=40, step=10)    # overall epsilon values after shuffling
 num_eps = eps.shape[-1]
 delta = 0.1
 sensitivity = 2.0 # range that X values take
-beta = np.random.normal(loc=0, scale=math.sqrt(math.sqrt(N)), size=K)
+beta = np.random.normal(loc=0, scale=math.sqrt(math.sqrt(N)), size=d)
 
-def run_dp(X, y, X_test, y_test):
-    dp_res = np.zeros(shape=num_eps)
-    for i in range(num_eps):
-        eps_0, delta_0 = local_budget(eps[i], delta, N, avg=False)
-        X_priv, y_priv = privatize(X, y, K, eps_0, delta_0, sensitivity, beta)
-        for j in range(num_trials):
-            dp_res[i] += probit_err(X_priv, y_priv, error, N, K, beta, X_test, y_test)
-        dp_res[i] /= num_trials
-    return dp_res
 
+# regular Probit regression
 def run_nondp(X, y, X_test, y_test):
     nondp_res = []
-    m = num_trials * num_eps
+    m = num_eps
     for i in range(m):
-        nondp_res.append(probit_err(X, y, error, N, K, beta, X_test, y_test))
+        params = run_probit(X, y)
+        nondp_res.append(predict(params, X_test, y_test))
     return max(nondp_res)
 
-def avg_params(X, y, eps_0, delta_0):
-    m = math.ceil(math.sqrt(N))
-    sum_params = np.zeros(shape=(K, 1))
-    for i in range(m):
-        X_priv, y_priv = privatize(X, y, K, eps_0, delta_0, sensitivity, beta)
-        _, _, params = run_probit(X_priv, y_priv)
-        #print("sigma 2", sigma2)
-        #params = label_priv_reg(X_priv, y_priv, math.sqrt(sigma2))
-        #print("params", params)
+# shuffling + averaging
+def averaging(X, y, eps_0, delta_0, k, beta, privll:bool):
+    sum_params = np.zeros(shape=(d, 1))
+    for i in range(k):
+        x_sigma2, y_sigma2 = noise_params(eps_0, delta_0, sensitivity, d)
+        X_priv, y_priv = privatize(X, y, beta, x_sigma2, y_sigma2, eps_0)
+        if privll:
+            params = optimize(X_priv, y_priv, math.sqrt(y_sigma2), x_sigma2).reshape(d, 1)
+        else:
+            params = run_probit(X_priv, y_priv)
         sum_params += params
-    avg_params = sum_params / (np.ones((K, 1)) * m)
+    avg_params = sum_params / (np.ones((d, 1)) * k)
     print("AVG PARAMS: ", avg_params)
     return avg_params
 
-def run_trials(X, y, X_test, y_test):
+# local Gaussion + shuffle
+def run_dp_shuffle(X, y, X_test, y_test, beta, avg:bool, sampling:bool, privll:bool):
+    dp_res = []
+    budget = []
+    for e in eps:
+        if avg:
+            eps_0, delta_0, k = local_budget_renyi(e, delta, N, avg=True)
+            avg_params = averaging(X, y, eps_0, delta_0, k, beta, privll)
+            score = predict(avg_params, X_test, y_test)
+            print(f"eps = {e}, score = {score}")
+        else:
+            if sampling:
+                sz = sample_sz(N, e, delta)
+                y_sigma2, x_sigma2 = sampling_params(sz, d, e, delta, sensitivity)
+                X_priv, y_priv = sampling_privatize(X, y, beta, y_sigma2, x_sigma2, sz)
+                #print(f"subsampling noise for y = {y_sigma2}")
+            else:
+                eps_0, delta_0, k = local_budget_renyi(e, delta, N, avg=False)
+                x_sigma2, y_sigma2 = noise_params(eps_0, delta_0, sensitivity, d)
+                X_priv, y_priv = privatize(X, y, beta, x_sigma2, y_sigma2, e)
+            if privll:
+                params = optimize(X_priv, y_priv, math.sqrt(y_sigma2), x_sigma2)
+            else:
+                params = run_probit(X_priv, y_priv)
+            score = predict(params, X_test, y_test)
+            print(f"eps = {e}, score = {score}")
+        dp_res.append(score)
+        budget.append((eps_0, delta_0))
+    return dp_res, budget
+
+
+# local Gaussion
+def run_dp_noshuffle(X, y, X_test, y_test, beta, privll:bool):
+    dp_res = []
+    for e in eps:
+        x_sigma2, y_sigma2 = noise_params(e, delta, sensitivity, d)
+        X_priv, y_priv = privatize(X, y, beta, x_sigma2, y_sigma2, e)
+        if privll:
+            params = optimize(X_priv, y_priv, math.sqrt(y_sigma2), x_sigma2)
+        else:
+            params = run_probit(X_priv, y_priv)
+        score = predict(params, X_test, y_test)
+        print(f"eps = {e}, score = {score}")
+        dp_res.append(score)
+    return dp_res
+
+def shuffle_trials(X, y, X_test, y_test, beta):
     nondp_res = run_nondp(X, y, X_test, y_test)
-    dp_res = run_dp(X, y, X_test, y_test)
+    shuffle_res, budget = run_dp_shuffle(X, y, X_test, y_test, beta, avg=False, sampling=False, privll=True)
+    print(f"non-DP = {nondp_res} \n", f"DP = {shuffle_res}")
+    return nondp_res, shuffle_res
 
-    plt.style.use('dark_background')
-    plt.plot(eps, dp_res, label='Local DP')
-    plt.plot(eps, (nondp_res * np.ones(shape=num_eps)), label='non-DP')
-    return nondp_res, dp_res
+def localdp_trials(X, y, X_test, y_test, beta):
+    probit_res = run_dp_noshuffle(X, y, X_test, y_test, beta, privll=False)
+    dp_res = run_dp_noshuffle(X, y, X_test, y_test, beta, privll=True)
+    print(f"Local with Probit = {probit_res} \n", f"Local with DP LL = {dp_res}")
+    return probit_res, dp_res
 
-def run_averaged_trials(X, y, X_test, y_test):
-    avg_dp_res = np.zeros(shape=num_eps)
-    for i in range(num_eps):
-        for j in range(num_trials):
-            eps_0, delta_0 = local_budget(eps[i], delta, N, avg=True)
-            dp_params = avg_params(X, y, eps_0, delta_0)
-            avg_dp_res[i] += predict(dp_params, N, X_test, y_test)
-        avg_dp_res[i] /= num_trials
+def ll_trials(X, y, X_test, y_test, beta):
+    probit_ll, budget = run_dp_shuffle(X, y, X_test, y_test, beta, avg=False, sampling=False, privll=False)
+    dp_ll, _ = run_dp_shuffle(X, y, X_test, y_test, beta, avg=False, sampling=False, privll=True)
+    print(f"probit log-likelihood = {probit_ll} \n", f"DP log-likelihood = {dp_ll}")
+    return probit_ll, dp_ll, budget
 
-    plt.style.use('dark_background')
-    plt.plot(eps, avg_dp_res, label='Local DP (averaged)')
-    return avg_dp_res
+def avg_trials(X, y, X_test, y_test, beta):
+    #nonavg = run_dp(X, y, X_test, y_test, beta, avg=False, sampling=False, privll=True)
+    #samp = run_dp(X, y, X_test, y_test, beta, avg=False, sampling=True, privll=True)
+    avg = run_dp_shuffle(X, y, X_test, y_test, beta, avg=True, sampling=False, privll=True)
+    print(f"averaging = {avg}")
+    return avg
+
+def samp_trials(X, y, X_test, y_test, beta):
+    samp = run_dp_shuffle(X, y, X_test, y_test, beta, avg=False, sampling=True, privll=True)
+    #print(f"sampling = {samp}")
+    return samp
 
 def run_dp_sgd():
-    X, y = generate(N, K, beta)
-    X_test, y_test = generate(N, K, beta)
+    X, y = generate(N, d, beta)
+    X_test, y_test = generate(N, d, beta)
     train_loader = torch_data(X, y)
     test_loader = torch_data(X_test, y_test)
 
     central_acc = []
     for i in range(num_eps):
-        central_acc.append(dp_sgd(train_loader, test_loader, eps[i], delta, K, central=True))
+        central_acc.append(dp_sgd(train_loader, test_loader, eps[i], delta, d, central=True))
 
     plt.plot(eps, central_acc, label='DP-SGD')
     return central_acc
 
 
+nondp = []
+probitll = []
+dpll = []
+avg = []
+samp = []
+shuffle = []
+local_dp = []
+local_prob = []
 
-X, y = generate(N, K, beta)
-X_test, y_test = generate(N, K, beta)
+for i in range(num_trials):
+    X, y = generate(N, d, beta)
+    X_test, y_test = generate(N, d, beta)
+    nondp.append(run_nondp(X, y, X_test, y_test))
+    #nondp, dp = shuffle_trials(X, y, X_test, y_test, beta)
+    localprob, localdp = localdp_trials(X, y, X_test, y_test, beta)
+    #probitll_res, dpll_res, budget = ll_trials(X, y, X_test, y_test, beta)
+    #avg_res = avg_trials(X, y, X_test, y_test, beta)
+    #samp_res = samp_trials(X, y, X_test, y_test, beta)
 
-nondp_res, dp_res = run_trials(X, y, X_test, y_test)
-avg_dp_res = run_averaged_trials(X, y, X_test, y_test)
+    print(f'''round {i}
+        Non-DP: {max(nondp)},
+        Local Gaussian with Probit: {max(localprob)},
+        Local Gaussian with Custom LL: {max(localdp)},
+    ''')
+
+    local_dp.append(localdp)
+    local_prob.append(localprob)
+    #shuffle.append(dp_shuffle)
+    # probitll.append(probitll_res)
+    # dpll.append(dpll_res)
+    #avg.append(avg_res)
+    #samp.append(samp_res)
+
+nondp_mean = np.mean(nondp)
+#nondp_err = np.std(nondp, axis=0)
+
+localdp_mean = np.mean(local_dp, axis=0)
+localdp_err = np.std(local_dp, axis=0)
+
+localprob_mean = np.mean(local_prob, axis=0)
+localprob_err = np.std(local_prob, axis=0)
+
+# shuffle_mean = np.mean(shuffle, axis=0)
+# shuffle_err = np.std(shuffle, axis=0)
+
+# probit_mean = np.mean(probitll, axis=0)
+# probit_err = np.std(probitll, axis=0)
+
+# dp_mean = np.mean(dpll, axis=0)
+# dp_err = np.std(dpll, axis=0)
+
+# avg_mean = np.mean(avg, axis=0)
+# avg_err = np.std(avg, axis=0)
+
+# samp_mean = np.mean(samp, axis=0)
+# samp_err = np.std(samp, axis=0)
+# results = {'eps': eps, 'local budget': budget, 'DP score': dp_mean, 'Probit score': probit_mean}
+# df = pd.DataFrame(results)
+# print(df)
+
+plt.errorbar(eps, localdp_mean, yerr=localdp_err, markersize=8, capsize=10, label="Local DP with Custom")
+plt.errorbar(eps, localprob_mean, yerr=localprob_err, markersize=8, capsize=10, label="Local DP with Probit")
+#plt.errorbar(eps, shuffle_mean, yerr=shuffle_err, markersize=8, capsize=10, label="Shuffling")
+# plt.errorbar(eps, probit_mean, yerr=probit_err, markersize=8, capsize=10, label="Probit LL")
+# plt.errorbar(eps, dp_mean, yerr=dp_err, markersize=8, capsize=10, label="DP LL")
+#plt.errorbar(eps, avg_mean, yerr=avg_err, markersize=8, capsize=10, label="Averaging")
+#plt.errorbar(eps, samp_mean, yerr=samp_err, markersize=8, capsize=10, label="Sampling")
+plt.plot(eps, (nondp_mean * np.ones(shape=num_eps)), label="Non-DP")
+
 #central_acc = run_dp_sgd()
 
-print("NONDP RESULT: ", nondp_res)
-print("DP RESULT: ",dp_res)
-print("DP WITH AVERAGING: ", avg_dp_res)
+# print(f'''Experiment results:
+#     non-privatized: {max(nondp)}
+#     probit log-like: {max(probitll)}
+#     private log-like: {max(dpll)}
+#     averaging: {max(avg)}
+#     subsampling: {max(samp)}
+# ''')
 
-plt.title(f"n = {N}, d = {K}")
+
+plt.title(f"Private X and Y: n = {N}, d = {d}")
 plt.xlabel('espilon')
 plt.ylabel('classification error')
 plt.legend()
 plt.show()
-
-#print("DP-SGD RESULT: ", central_acc)
-
-# print("BETA:", beta)
--- mechanisms.py
+++ mechanisms.py
@@ -2,10 +2,12 @@
 baseline mechanisms for differentially private and nonprivate probit regression
 '''
 
+from threading import local
 import numpy as np
 import statsmodels.api as smf
 import math
 import pandas as pd
+import random
 from sklearn.metrics import accuracy_score
 
 DELTA1 = 1e-5    # intermediate delta for shuffling, delta \in [0,1]
@@ -13,9 +15,10 @@ DELTA1 = 1e-5    # intermediate delta for shuffling, delta \in [0,1]
 DELTA2 = 1e-5    # intermediate delta for k-composition
 
 
-def predict(beta_hat, N, X_test, y_test):
-    y_pred = np.empty(shape=N)
-    for i in range(N):
+def predict(beta_hat, X_test, y_test):
+    n = np.shape(y_test)[0]
+    y_pred = np.empty(shape=n)
+    for i in range(n):
         rho = np.random.standard_normal()
         xbeta = np.dot(X_test[i], beta_hat)
         if xbeta + rho > 0:
@@ -30,64 +33,125 @@ def run_probit(X, y):
     #print(result.summary2())
     params = pd.DataFrame(result.params,columns={'coef'},).to_numpy()
     #print(probit_model.hessian(params))
-    return probit_model, result, params
-
-# runs probit regression and returns error metric
-def probit_err(X, y, error, N, K, beta, X_test, y_test):
-    _, result, params = run_probit(X, y)
-    print(params)
-    if error == "LOG-LIKE":
-        return result.llf / N
-    elif error == "BETA-NORM":
-        print(np.linalg.norm(beta - params.T))
-        return np.linalg.norm(beta - params.T)
-    elif error == "PRED":
-        return predict(params, N, X_test, y_test)
+    return params
 
 
-def priv_params(K, sensitivity, eps, delta, beta):
-    x_sigma2 = ((K*sensitivity)**2/eps**2)*(2.0*math.log(1.25/delta))
-    gamma = 1/math.sqrt(1+(x_sigma2*np.linalg.norm(np.square(beta))))
+def noise_params(eps, delta, sensitivity, d):
+    eps /= 2
+    x_sigma2 = ((d*sensitivity)**2/eps**2)*(2.0*math.log(1.25/delta))
     y_sigma2 = (1/eps**2)*(2.0*math.log(1.25/delta))    # sensitivity of y's = 1
-    return x_sigma2, gamma, y_sigma2
+    return x_sigma2, y_sigma2
+
+def rr(eps, yi):
+    # wp 1-p, output true answer
+    # otherwise, output 1 or 0 with even chance
+    p = 2 / (math.exp(eps) + 1)
+    if (random.uniform(0, 1) < p):
+        if (np.random.randint(2) == 1):
+            return 1
+        else:
+            return 0
+    else:
+        return yi
+
 
 # add Gaussian noise to X's and y's
-def privatize(X, y, K, eps, delta, sensitivity, beta):
-    N = np.shape(y)[0]
-    x_sigma2, gamma, y_sigma2 = priv_params(K, sensitivity, eps, delta, beta)
+def privatize(X, y, beta, x_sigma2, y_sigma2):
+    n = np.shape(y)[0]
+    d = np.shape(X)[1]
+    gamma = 1/math.sqrt(1+(x_sigma2*np.linalg.norm(np.square(beta))))
     
     X_priv = np.copy(X)
-    y_priv = np.zeros(N)
+    y_priv = np.zeros(n)
 
-    for i in range(N):
-        x_noise = np.random.normal(loc=0, scale=math.sqrt(x_sigma2), size=K)
+    for i in range(n):
+        x_noise = np.random.normal(loc=0, scale=math.sqrt(x_sigma2), size=d)
         X_priv[i] += x_noise
-        X_priv[i] *= gamma
+        #X_priv[i] *= gamma
 
         y_noise = np.random.normal(loc=0, scale=math.sqrt(y_sigma2))
         if y_noise + float(y[i]) >= 0.5:
             y_priv[i] += 1
 
-    return X_priv, y
+    return X_priv, y_priv
 
-# returns (ε_0, δ_0) for local randomizer
-def local_budget(eps, delta, n, avg: bool):
+# add Gaussian noise to X's and y's
+def privatize_rr(X, y, beta, x_sigma2, eps):
+    n = np.shape(y)[0]
+    d = np.shape(X)[1]
+    
+    X_priv = np.copy(X)
+    y_priv = np.zeros(n)
+
+    for i in range(n):
+        x_noise = np.random.normal(loc=0, scale=math.sqrt(x_sigma2), size=d)
+        X_priv[i] += x_noise
+
+        y_priv[i] = rr(eps/2, y[i])
+
+    return X_priv, y_priv
+
+
+# returns (ε_0, δ_0) for local randomizer using renyi dp shuffle
+def local_budget_renyi(eps, delta, n, avg:bool):
+    alpha = max(2, n / (9 * math.exp(5 * eps)))
+    print(f"alpha = {alpha}")
+    s = ((math.log(1/delta) + (alpha-1)*math.log(1-(1/alpha)) - math.log(alpha)) / (alpha - 1))
+    print(f"subtracting {s}")
+    eps_renyi = eps - ((math.log(1/delta) + (alpha-1)*math.log(1-(1/alpha)) - math.log(alpha)) / (alpha - 1))
+    print(f"ε(lambda) = {eps_renyi}")
+    delta_renyi = (1 - (1/alpha))**alpha * math.exp((alpha - 1)*(eps_renyi - eps)) / (alpha - 1)
+    print(f"δ(lambda) = {delta_renyi}")
     if avg:
-        k = math.log(math.sqrt(n))
-        eps_comp_factor = (math.sqrt(2) * (math.sqrt(k*math.log(1/DELTA2) + 2*k) 
-                                    - math.sqrt(k*math.log(1/DELTA2)))) / (2*k)
-        eps *= eps_comp_factor
-        old_eps = eps*math.sqrt(2*k*math.log(1/DELTA2)) + (k*eps*(math.exp(eps)-1))
-        delta = (delta - DELTA2) / k
-        print(f"after averaging: eps = {old_eps}, eps' = {eps}, delta' = {delta}")
-
-    eps_shuffle_factor = 2*math.log( (math.sqrt(n + 4*math.log(1/delta)) + math.sqrt(n)) / (2*math.log(1/delta)) )
-    eps_0 = eps * eps_shuffle_factor
-    delta_0 = (delta - DELTA1) / (n * (math.exp(eps) + 1) * (1 + math.exp(-1 * eps_0/2)))
-
-    if avg:
+        eps_0 = eps
+        k = math.ceil(eps_renyi / ((1/(alpha-1))*math.log(1 + math.comb(alpha, 2) * 4 * (math.exp(eps_0)-1)**2 / n)))
+        print(f"number of rounds for averaging = {k}")
+        delta_0 = max(1e-13, delta - delta_renyi) / (k*n*(math.exp(eps) + 1)*(1 + math.exp(-1 * eps_0)/2))
         print(f"(ε_0, δ_0) for averaged trials = ({eps_0}, {delta_0})")
     else:
-        print(f"(ε_0, δ_0) without averaging = ({eps_0}, {delta_0})")
-    
-    return eps_0, delta_0
+        eps_0 = math.log(math.sqrt(n * (math.exp((alpha-1)*(eps_renyi)) - 1) / (math.comb(alpha, 2) * 4)) + 1)
+        delta_0 = (max(1e-13, delta - delta_renyi)) / (n*(math.exp(eps) + 1)*(1 + math.exp(-1 * eps_0)/2))
+        k = 1
+        print(f"(ε_0, δ_0) without averaging = ({eps_0}, {delta_0})")    
+    return eps_0, delta_0, k
+
+
+local_budget_renyi(eps=5, delta=0.1, n=400, avg=False)
+
+# return sampling size
+def sample_sz(n, eps, delta):
+    m = 1
+    km = 0
+    for k in range(n**2):
+        fac = m*((math.exp(eps) - 1) / n)
+        #k = eps * math.log(1 + fac)
+        delta_p = delta - (k*delta/(n*math.log(n)))
+        if delta_p < 0:
+            break
+        if math.log(1 + fac) * math.sqrt(2*k*math.log(1/delta_p)) + k*math.log(1 + fac)*fac > eps:
+            continue
+        if k * m > km:
+            km = k*m
+    print(f"sampling size = {km}")
+    return km
+
+# privatization for subsampling method
+def sampling_params(n, d, eps, delta, sensitivity):
+    half_delta = delta / 2
+    half_eps = eps / 2
+    y_sigma2 = n**2 * math.log(1/half_delta) * math.log(1/half_delta) / half_eps**2
+    x_sigma2 = d * sensitivity**2 * y_sigma2
+    return y_sigma2, x_sigma2
+
+def sampling_privatize(X, y, beta, y_sigma2, x_sigma2, sz):
+    n = np.shape(X)[0]
+    d = np.shape(X)[1]
+    sample = np.random.randint(0, n-1, size=sz)
+    X_sample = np.empty((sz, d))
+    y_sample = np.empty(sz)
+    for i in range(sz):
+        indx = sample[i]
+        X_sample[i] = X[indx]
+        y_sample[i] = y[indx] 
+    X_priv, y_priv = privatize(X_sample, y_sample, beta, x_sigma2, y_sigma2)
+    return X_priv, y_priv
