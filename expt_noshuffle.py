0053fae1915502703e334d31d86eaef8da54f1f9
Jamie Lu <jamielu@college.harvard.edu>
2023-11-07 21:52:19 -0500

    noise expmts

--- expt_noshuffle.py
+++ expt_noshuffle.py
@@ -7,10 +7,11 @@ tries different values of the privacy parameter
 from generate_data import generate
 from probit import predict, run_probit
 from mechanisms import noise_params, privatize, privatize_rr
-from priv_opt import optimize, optimize_x
+from priv_opt import optimize_rr, optimize_x
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import random
 
 # initialize parameters
 d = 5   # dimensions
@@ -32,68 +33,117 @@ def run_nondp(X, y, X_test, y_test):
         nondp_res.append(predict(params, X_test, y_test))
     return max(nondp_res)
 
-def run_dp_noshuffle(X, y, X_test, y_test, beta, rr:bool):
+def run_dp_noshuffle(X, y, X_test, y_test, beta, d, prob:bool):
     dp_res = []
     for e in eps:
         x_sigma2, y_sigma2 = noise_params(e, delta, sensitivity, d)
-        if rr:
-            X_priv, y_priv = privatize_rr(X, y, beta, x_sigma2, e)
+        X_priv, y_priv = privatize_rr(X, y, x_sigma2, e)
+        if prob:
+            params = run_probit(X_priv, y_priv)
         else:
-            X_priv, y_priv = privatize(X, y, beta, x_sigma2, y_sigma2)
-        params = optimize(X_priv, y_priv, math.sqrt(y_sigma2), x_sigma2)
+            params = optimize_rr(X_priv, y_priv, math.sqrt(y_sigma2), x_sigma2)
         score = predict(params, X_test, y_test)
-        print(f"eps = {e}, delta = {delta}, score = {score}, rr = {rr}")
+        print(f"n = {np.shape(y)[0]}, d = {d}, eps = {e}, delta = {delta}, score = {score}, probit = {prob}")
         dp_res.append(score)
     return dp_res
 
-def run_dp_x(X, y, X_test, y_test, beta):
-    dp_res = []
-    for e in eps:
-        x_sigma2, _ = noise_params(e, delta, sensitivity, d)
-        X_priv, _ = privatize(X, y, beta, x_sigma2, 0)
-        params = optimize_x(X_priv, y, x_sigma2)
-        score = predict(params, X_test, y_test)
-        print(f"eps = {e}, delta = {delta}, score = {score}")
-        dp_res.append(score)
-    return dp_res
+# def run_dp_x(X, y, X_test, y_test, beta, d):
+#     dp_res = []
+#     for e in eps:
+#         x_sigma2, _ = noise_params(e, delta, sensitivity, d)
+#         X_priv, _ = privatize(X, y, beta, x_sigma2, 0)
+#         params = optimize_x(X_priv, y, x_sigma2)
+#         score = predict(params, X_test, y_test)
+#         print(f"eps = {e}, delta = {delta}, score = {score}")
+#         dp_res.append(score)
+#     return dp_res
+
+def rr(py, yi):
+    '''
+    with probability py, output yi, otherwise flip
+    '''
+    if (random.uniform(0, 1) < py):
+        return yi
+    else:
+        return 1 - yi
+
+def priv_rr(X, y, sigma2, py):
+    '''
+    gaussin noise for x's, randomized response for y's
+    sigma2 = scale of gaussian noise
+    py = probability that we output true y
+    '''
+    n = np.shape(y)[0]
+    d = np.shape(X)[1]
+    
+    X_priv = np.copy(X)
+    y_priv = np.zeros(n)
+
+    for i in range(n):
+        x_noise = np.random.normal(loc=0, scale=math.sqrt(sigma2), size=d)
+        X_priv[i] += x_noise
+
+        y_priv[i] = rr(py, y[i])
+
+    return X_priv, y_priv
+    
+
+def run_fixed_noise(X, y, X_test, y_test, sigma2, py):
+    X_priv, y_priv = priv_rr(X, y, sigma2, py)
+    params = optimize_rr(X_priv, y_priv, py, sigma2)
+    score = predict(params, X_test, y_test)
+    print(f"n = {np.shape(y)[0]}, d = {d}, sigma2 = {sigma2}, py = {py}, score = {score}")
+    return score
+    
+# Define parameters
+N = np.arange(start=10000, stop=100000, step=10000)
+D = [5, 10, 25]
+sigma2 = np.arange(start=0, stop=1, step=0.1)[::-1] # reversed [1, 0.9,..., 0.1, 0]
+py = math.exp(2) / (1 + math.exp(2)) # fixing epsilon_y = 2
+num_trials = 3
 
 
+min_noise = {}
+results = {}
+fig = plt.figure()
+idx = 1
 
-
-for N in [600]:
+for n in N:
     delta = 1/N
-    beta = np.random.normal(loc=0, scale=math.sqrt(math.sqrt(N)), size=d)
-    nondp = []
-    rr = []
-    x_res = []
-    for i in range(num_trials):
-        X, y = generate(N, d, beta)
-        X_test, y_test = generate(N, d, beta)
-        nondp.append(run_nondp(X, y, X_test, y_test))
-        xy = run_dp_noshuffle(X, y, X_test, y_test, beta, rr=True)
-        x = run_dp_x(X, y, X_test, y_test, beta)
-        x_res.append(x)
-        rr.append(xy)
+    for d in D:
+        beta = np.random.normal(loc=0, scale=math.sqrt(math.sqrt(n)), size=d)
+        X, y = generate(n, d, beta)
+        X_test, y_test = generate(n, d, beta)
+        
+        scores = []
+        sigma2 = 1
+        min_sigma2 = 0
+        r = False
+        while sigma2 >= 0: 
+            score = 0
+            for i in range(num_trials):
+                score += run_fixed_noise(X, y, X_test, y_test, sigma2, py)
+            score /= num_trials
+            if score > 0.6 and not r: # benchmark score
+                min_sigma2 = sigma2
+                r = True
+            sigma2 -= 0.1
+            scores.append(score)
+        print(f"(n, d) = ({np.shape(y)[0]}, {d}), scores = {scores}")
+        min_noise[(n, d)] = min_sigma2
+        results[(n, d)] = scores
+        
+        ax = fig.add_subplot(10,3,idx)
+        idx += 1
+        ax.title(f"n = {N}, d = {d}, min_sigma2 = {min_sigma2}")
+        ax.plot(np.arange(0, 1, 0.1), scores)
+        ax.xlabel('Gaussian noise σ^2')
+        ax.ylabel('Classification error')
 
-        print(f'''round {i}
-            Non-DP: {max(nondp)},
-            Privatize X, Y (RR): {xy},
-            Privatize X results: {x}
-        ''')
-
-    nondp_mean = np.mean(nondp)
-
-    rr_mean = np.mean(rr, axis=0)
-    rr_err = np.std(rr, axis=0)
-    x_mean = np.mean(x_res, axis=0)
-    x_err = np.std(x_res, axis=0)
-
-    plt.plot(eps, (nondp_mean * np.ones(shape=num_eps)), label="Non-DP")
-    plt.errorbar(eps, x_mean, yerr=x_err, markersize=8, capsize=10, label="Privatize X")
-    plt.errorbar(eps, rr_mean, yerr=rr_err, markersize=8, capsize=10, label="RR")
-
-    plt.title(f"Privatie X, no shufle, n = {N}, d = {d}, delta = {delta}")
-    plt.xlabel('espilon')
-    plt.ylabel('classification error')
-    plt.legend()
-    plt.show()
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.4)
+plt.show()
